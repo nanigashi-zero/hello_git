ebf92b5beb214c3d15f62b37ca5820a7b50cbac5
nanigashi-zero <nanigashi08x@gmail.com>
2020-04-07 20:59:59 +0900

    二次元配列を表示する

--- tutorial.py
+++ tutorial.py
@@ -1,2 +1,21 @@
 print("hello Github")
 print("good night")
+
+i = 4
+j = 4
+
+#内包表記で二次元配列tetrisを定義
+tetris = [['' for count_1 in range (4)] for count_2 in range(i)]
+
+#tetris 4×4行列の値初期化
+for a in range(i):
+    for b in range(j):
+        tetris[a][b] = '1'
+
+#tetris 行列内の文字列表示
+for a in range(i):
+    tetris_array = ''
+    for b in range(j):
+        tetris_array += tetris[a][b]
+    print(tetris_array)
+        
